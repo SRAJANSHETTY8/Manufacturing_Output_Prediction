365a618adc7b7adf61904726ad8be218973dbd0d
SRAJANSHETTY8 <shettysrajan760@gmail.com>
2026-02-06 17:13:54 +0530

    Manufacturing Output Prediction Project

---
+++ frontend/app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import requests
+import pickle
+
+st.title("Manufacturing Output Prediction")
+
+features = pickle.load(open("../model/features.pkl","rb"))
+
+inputs = []
+
+for feature in features:
+    val = st.number_input(feature)
+    inputs.append(val)
+
+if st.button("Predict"):
+
+    response = requests.post(
+        "http://127.0.0.1:8000/predict",
+        json={"data": inputs}
+    )
+
+    st.write("Prediction:", response.json())
